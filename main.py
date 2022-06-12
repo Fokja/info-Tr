9ba561f5f95eae28014b0a43446eeebdab5eeb01
Elphador <96861627+Elphador@users.noreply.github.com>
2022-06-12 22:30:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from pyrogram import Client
 
 Bot = Client(
     "Translator Bot",
-    bot_token = os.getenv ["BOT_TOKEN"],
-    api_id = int(os.getenv ["API_ID"]),
-    api_hash = os.getenv ["API_HASH"],
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
     plugins = dict(root="plugins")
 )
+
 
 
 Bot.run()
